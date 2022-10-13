0434f0405975239d3b4d88257e4e86f2fc4191ab
Mona Lisa <giatran1222@gmail.com>
2022-10-12 18:24:04 -0700

    almost done with userRoutes just need to update some variable names and export model

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -16,20 +16,77 @@ router.post('/', async (req, res) => {
 });
 
 // CREATE new user
-router.post
+router.post('/', async (req, res) => {
+    try {
+        const userData = await this.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+        });
 
+        req.session.save(() => {
+            req.session.loggedIn = true;
 
+            res
+            .status(200)
+            .json({ user: userData, message: 'Welcome to FoodSavers!'});
+        });
 
+    } catch (err) {
+        res.status(500).json (err);
+    }
+});
 
 // Login
-router.post
+router.post ('/login', async (req, res) => {
+    try {
+        const userData = await this.findOne ({
+            where: {
+                email: req.body.email,
+            }
+        });
+
+        if (!userData) {
+            res
+              .status(400)
+              .json({ message: 'Incorrect email or password'});
+              return;
+        }
+
+        const validPassword = await userData.checkPassword(req.body.password);
 
 
+        if (!validPassword) {
+            res
+              .status(400)
+              .json ({ message: 'Incorrect email or password'});
+              return;
+        }
+
+        req.session.save(() => {
+            req.session.loggedIn = true;
+
+            res
+              .status(200)
+              .json({ user: userData, message: 'You are now logged in!' });
+        });
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 
 // Logout
-router.post
-
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+});
 
 
 module.exports = router;
