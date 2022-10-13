ece282af29d807cef0042f0d3e22b934a0597ee9
Mona Lisa <giatran1222@gmail.com>
2022-10-12 18:37:09 -0700

    almost done with ingredients and recipe routes

--- controllers/api/recipeRoutes.js
+++ controllers/api/recipeRoutes.js
@@ -1,10 +1,45 @@
 const router = require('express').Router();
+//need to add recipe model below
+
 
 // GET all recipes
+router.get('/', async (req, res) => {
+    try{
+        //change this to name of recipe model imported
+        const recipeData = await this.name.findAll ({
+            raw: true
+        });
+
+        // for now renders to homepage
+        res.render('homepage', {
+            recipes: recipeData
+        })
 
 
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 // GET one recipe
+router.get('/recipe/id:', async (req, res) => {
+    try {
+        const recipeData = await this.findByPk(req.params.id, {
+            raw: true
+        });
+
+        res.render ('homepage', {
+            recipe: recipeData
+        })
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// ADD recipe
+
+// DELETE recipes
 
 
 
