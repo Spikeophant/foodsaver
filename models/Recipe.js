e212055a9600e8915c1e457ff3e7bab33076028a
Dustin Davisson <dustin.davisson@gmail.com>
2022-10-12 17:24:31 -0700

    Add Recipe.js model.

---
+++ models/Recipe.js
@@ -0,0 +1,25 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Recipe extends Model {}
+
+Recipe.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    body: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    }
+  }
+);
+
+module.exports = Recipe;
