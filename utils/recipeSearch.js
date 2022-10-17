2800de20864fdcf60b98b233a598c79d646273d9
Dustin Davisson <dustin.davisson@gmail.com>
2022-10-17 16:47:29 -0700

    By ingredient or by id.

--- utils/recipeSearch.js
+++ utils/recipeSearch.js
@@ -1,17 +1,32 @@
 require('dotenv').config();
 
 const baseUrl = 'https://www.themealdb.com/api/json/v1/';
-const mealDBApiKey = 1;
+const mealDBApiKey = process.env.MEALDB_API_KEY2;
 const ingredientSearch = '/filter.php?i=';
+const singleRecipeSearch = '/lookup.php?i='
 
-async function getRecipe(ingredient) {
+async function getRecipeByIngredient(ingredient) {
   const res = await fetch(baseUrl + mealDBApiKey + ingredientSearch + ingredient);
   if (!res.ok) {
     console.log(res.status);
-    throw new Error('MealDB Lookup Failed!! Abort!!');
+    throw new Error('MealDB Search Failed!! Abort!!');
   }
-  return await res.json()
+  const recipes = await res.json();
+  console.log(recipes);
+  return recipes
 }
 
-console.log(getRecipe('chicken'));
+async function getRecipe(id) {
+  const res = await fetch(baseUrl + mealDBApiKey + singleRecipeSearch + id);
+  if (!res.ok) {
+    console.log(res.status);
+    throw new Error('MealDB Lookup Failed!! Abort!!')
+  }
+  const recipe = await res.json();
+  console.log(recipe.meals[0]);
+  return recipe.meals[0];
+}
 
+
+
+getRecipe(53039);
