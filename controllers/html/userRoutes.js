263bf6db4097b74e97380ca45b6eaad2a3eb5b15
Mona Lisa <giatran1222@gmail.com>
2022-10-19 17:18:38 -0700

    connecting handles and testing css

--- controllers/html/userRoutes.js
+++ controllers/html/userRoutes.js
@@ -3,7 +3,13 @@ const { User, Recipe } = require('../../models');
 
 router.get('/login', async (req, res) => {
   try {
+    if (req.session.logged_in) {
+      res.redirect('/');
+      return;
+    }
+
     res.render('login');
+
   } catch (err) {
     res.status(500).json(err);
   }
